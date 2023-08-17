5e6dd0bc59cf4cb962d0dd61969356f9cf885106
unknown <Tecnico@LAPTOP-IN7389U6.eprern.gov.ar>
2023-08-17 10:35:44 -0300

    Tarea completada

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const {Router} = require('express');
 const router = Router();
 
-const {getUsers, createUsers} = require('../controllers/index.controller')
+const {getUsers, createUsers, getUsersById, deleteUsers, updateUsers} = require('../controllers/index.controller')
 
 router.get('/users',getUsers);
-router.post('/users',createUsers)
+router.get('/users/:id', getUsersById);
+router.post('/users',createUsers);
+router.delete('/users/:id',deleteUsers);
+router.put('/users/:id',updateUsers)
 
 
 module.exports = router;
